0a1de1cb848fe3957dc66dfcbf6622db6b0eb3a5
Hiroaki-K4 <49no19@gmail.com>
2025-06-22 20:03:45 +0900

    Add ch05 folder

--- common/gridworld.py
+++ common/gridworld.py
@@ -58,3 +58,16 @@ class GridWorld:
         renderer = render_helper.Renderer(self.reward_map, self.goal_state,
                                         self.wall_state)
         renderer.render_v(v, policy, print_value)
+
+    def reset(self):
+        self.agent_state = self.start_state
+        return self.agent_state
+
+    def step(self, action):
+        state = self.agent_state
+        next_state = self.next_state(state, action)
+        reward = self.reward(state, action, next_state)
+        done = (next_state == self.goal_state)
+
+        self.agent_state = next_state
+        return next_state, reward, done
