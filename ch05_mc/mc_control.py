31c2224d46fe3c0146e156b5492cfd7f665c5365
Hiroaki-K4 <49no19@gmail.com>
2025-07-08 08:38:53 +0900

    Update mc_control.py

--- ch05_mc/mc_control.py
+++ ch05_mc/mc_control.py
@@ -6,6 +6,7 @@ if "__file__" in globals():
 from collections import defaultdict
 
 import numpy as np
+from common.gridworld import GridWorld
 
 
 def greedy_probs(Q, state, epsilon=0, action_size=4):
@@ -21,12 +22,13 @@ def greedy_probs(Q, state, epsilon=0, action_size=4):
 class McAgent:
     def __init__(self):
         self.gamma = 0.9
+        self.epsilon = 0.1
+        self.alpha = 0.1
         self.action_size = 4
 
         random_actions = {0: 0.25, 1: 0.25, 2: 0.25, 3: 0.25}
         self.pi = defaultdict(lambda: random_actions)
         self.Q = defaultdict(lambda: 0)
-        self.cnts = defaultdict(lambda: 0)
         self.memory = []
 
     def get_action(self, state):
@@ -48,7 +50,28 @@ class McAgent:
             state, action, reward = data
             G = self.gamma * G + reward
             key = (state, action)
-            self.cnts[key] += 1
-            self.Q[key] += (G - self.Q[key]) / self.cnts[key]
-            self.pi[state] = greedy_probs(self.Q, state)
+            self.Q[key] += (G - self.Q[key]) * self.alpha
+            self.pi[state] = greedy_probs(self.Q, state, self.epsilon)
 
+
+if __name__ == "__main__":
+    env = GridWorld()
+    agent = McAgent()
+
+    episodes = 10000
+    for episode in range(episodes):
+        state = env.reset()
+        agent.reset()
+
+        while True:
+            action = agent.get_action(state)
+            next_state, reward, done = env.step(action)
+
+            agent.add(state, action, reward)
+            if done:
+                agent.update()
+                break
+
+            state = next_state
+
+    env.render_q(agent.Q)
