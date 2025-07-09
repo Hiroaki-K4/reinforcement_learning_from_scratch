9d3c0953e68d9ddf9206883e44b9477226ec220a
Hiroaki-K4 <49no19@gmail.com>
2025-07-10 08:32:12 +0900

    Add importance sampling

--- ch05_mc/importance_sampling.py
+++ ch05_mc/importance_sampling.py
@@ -7,6 +7,7 @@ pi = np.array([0.1, 0.1, 0.8])
 e = np.sum(x * pi)
 print("E_pi[x]", e)
 
+# Montecalro
 n = 100
 samples = []
 for _ in range(n):
@@ -16,3 +17,20 @@ for _ in range(n):
 mean = np.mean(samples)
 var = np.var(samples)
 print("MC: {:.2f} (var: {:.2f})".format(mean, var))
+
+
+# Importance sampling
+b = np.array([0.2, 0.2, 0.6])
+n = 100
+samples = []
+
+for _ in range(n):
+    idx = np.arange(len(b))
+    i = np.random.choice(idx, p=b)
+    s = x[i]
+    rho = pi[i] / b[i]
+    samples.append(rho * s)
+
+mean = np.mean(samples)
+var = np.var(samples)
+print("IS: {:.2f} (var: {:.2f})".format(mean, var))
--- ch05_mc/mc_control.py
+++ ch05_mc/mc_control.py
@@ -15,7 +15,7 @@ def greedy_probs(Q, state, epsilon=0, action_size=4):
 
     base_prob = epsilon / action_size
     action_probs = {action: base_prob for action in range(action_size)}
-    action_probs[max_action] += (1 - epsilon)
+    action_probs[max_action] += 1 - epsilon
     return action_probs
 
 
